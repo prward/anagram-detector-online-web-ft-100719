57629a736c7579fec5b48ae3e86a394731ab531b
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-17 13:56:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   end
   
   def match(array_words)
-    array_words.find_all
+    array_words.find_all do |word|
     if word.split("").sort == self.array_words.split("").sort 
         word 
   end
