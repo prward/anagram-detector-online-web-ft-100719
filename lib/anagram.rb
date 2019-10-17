9eab03b136ad455b37d3313faeb4e7df687cb99e
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-17 13:57:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
 class Anagram
   
-  attr_accessor :word
+  attr_accessor :word_analyzed
   
   def initialize(word)
-    @word = word
+    @word_analyzed = word_analyzed
   end
   
   def match(array_words)
     array_words.find_all do |word|
-    if word.split("").sort == self.word.split("").sort 
+    if word.split("").sort == self.word_analyzed.split("").sort 
         word 
   end
 end
