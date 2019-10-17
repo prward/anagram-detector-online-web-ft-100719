e09e32bb0b7e29dd754679d3be7b9b492554a30f
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-17 13:59:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,21 +1,15 @@
 # Your code goes here!
 class Anagram
-  
-  attr_accessor :word_analyzed
-  
+
+  attr_accessor :word
+
   def initialize(word)
-    @word_analyzed = word_analyzed
+    @word = word
   end
-  
-  def match(array_words)
-    array_words.find_all do |word|
-    if word.split("").sort == self.word_analyzed.split("").sort 
-        word_analyzed 
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
   end
 end
-end
-end
-
-
-listen = Anagram.new("listen")
-listen.match(%w(enlists google inlets banana))
